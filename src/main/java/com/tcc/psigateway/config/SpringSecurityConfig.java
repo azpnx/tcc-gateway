14b271c5d597e09f9ec867296d8678abfde579ed
Gabriel <gabrieldouradogarcez@gmail.com>
2022-08-10 16:36:31 -0300

    ResourceServer adicionado

---
+++ src/main/java/com/tcc/psigateway/config/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.tcc.psigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http){
+        return http.authorizeExchange()
+                .pathMatchers("/api/security/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/security/usuarios").hasAnyRole("ADMIN","USER")
+                .anyExchange().authenticated()
+                .and().csrf().disable()
+                .build();
+    }
+}
