fcd3859c43f7eb00d3d4518b90b521013143dda8
Gabriel <gabrieldouradogarcez@gmail.com>
2022-08-10 21:16:24 -0300

    Alterando algumas rotas

--- src/main/java/com/tcc/psigateway/config/SpringSecurityConfig.java
+++ src/main/java/com/tcc/psigateway/config/SpringSecurityConfig.java
@@ -1,21 +1,27 @@
 package com.tcc.psigateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SpringSecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http){
         return http.authorizeExchange()
                 .pathMatchers("/api/security/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.POST,"/api/usuarios/**").permitAll()
                 .pathMatchers(HttpMethod.GET, "/api/security/usuarios").hasAnyRole("ADMIN","USER")
                 .anyExchange().authenticated()
-                .and().csrf().disable()
+                .and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable()
                 .build();
     }
 }
